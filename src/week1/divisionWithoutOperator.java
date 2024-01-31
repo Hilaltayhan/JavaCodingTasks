503d95d2aa43c8f881c82066d46240b17e68135d
hilal <hilaltayhan95@gmail.com>
2024-01-31 18:06:24 +0000

    add divisionWithoutOperator.java and oddOrEven

--- src/week1/divisionWithoutOperator.java
+++ src/week1/divisionWithoutOperator.java
@@ -1,4 +1,16 @@
 package week1;
 
 public class divisionWithoutOperator {
+
+    public static void DivisionWithoutOperator(int number1, int number2){
+
+
+    }
+
+
 }
+
+
+/*
+write a method that can divide two numbers without using division operator
+ */
