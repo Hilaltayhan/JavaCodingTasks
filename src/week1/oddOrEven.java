1f0470b7f70f38e85ffe86eb3e34628bff957b2d
Turhan Ismailov <turhan.ismailov@gmail.com>
2024-02-01 11:20:25 +0000

    JavaCodingTasks week1 tasks: divisionWithoutOperator.java Finra.java oddOrEven.java Week1.txt

--- src/week1/oddOrEven.java
+++ src/week1/oddOrEven.java
@@ -1,4 +1,35 @@
 package week1;
 
+import java.util.Scanner;
+
 public class oddOrEven {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(identify(input.nextInt()));
+
+        //OR   // System.out.println(identify(6));
+    }
+
+    public static String identify(int number) {
+
+        return  (number % 2 == 0) ? "Even" : "Odd";
+
+     /*//OR
+     if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+      */
+
+    }
 }
+/*1-  Numbers -- odd & even
+
+Write a method that can identify  a given number is even or odd.
+EX:
+       identify(5) --> "Odd"
+       identify(6) --> "Even"
+       */
