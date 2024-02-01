f703737a40d2b707d23aa31fd2d7f5796a7c69ed
Christopher <148374799+Christopher-To@users.noreply.github.com>
2024-02-01 22:29:09 +0000

    OddOrEven task done.

--- src/Christopher/week1/OldOrEven.java
+++ src/Christopher/week1/OldOrEven.java
@@ -1,4 +1,27 @@
 package Christopher.week1;
 
+import java.util.Scanner;
+
 public class OldOrEven {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Your number " + num + " is even");
+        }else {
+            System.out.println("Your number " + num + " is odd");
+        }
+        input.close();
+
+
+    }
+
 }
+/*Write a method that can identify  a given number is even or odd.
+        EX:
+identify(5) --> "Odd"
+identify(6) --> "Even"*/
