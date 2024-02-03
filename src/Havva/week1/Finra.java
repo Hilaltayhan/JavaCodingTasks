3d169b11ea1e572aae9d2b125477aa47b16468a7
havvasengel <havvasengel@yahoo.com>
2024-02-03 00:12:38 +0000

    create 3 java files

---
+++ src/Havva/week1/Finra.java
@@ -0,0 +1,37 @@
+package Havva.week1;
+
+public class Finra {
+    /*Write a method that prints out the numbers from 1 to 30 but;
+for numbers which are multiple of 3 print "FIN" instead of the number,
+for numbers which are multiple of 5, print "RA" instead of the number,
+and for numbers which are multiple of both 3 and 5, print "FINRA" instead of the number.
+
+     */
+    public static void finra(int num) {
+        for (int i = 1; i <= num; i++) {
+            if (i % 15 == 0) {//divisible by 3 and 5
+
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {// divisible by 3
+                System.out.println("FIN");
+
+            } else if (i % 5 == 0) {//divisible by 5
+
+                System.out.println("RA");
+            }
+                else{
+                    // Number is not divisible by 3 or 5
+                    System.out.println(i);
+                }
+            }
+        }
+
+        public static void main (String[]args){
+            finra(30);
+
+        }
+
+
+}
+
+
