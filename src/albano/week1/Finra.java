4a57ad6b193ec5eecbe09b14b52aef2a49dc5028
Almpano <bani_098@hotmail.com>
2024-02-03 16:17:37 +0000

    Created the method "finra" and worked on it. Also created the main method, created and a Scanner Object so the user can put the the number for the "finra" method from his keybord. And tested the method "finra" with calling it.

--- src/albano/week1/Finra.java
+++ src/albano/week1/Finra.java
@@ -1,5 +1,44 @@
 package albano.week1;
 
+import java.util.Scanner;
+
 public class Finra {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a number bigger tha 30! Thank you for your corporation :D");
+        int n = scanner.nextInt();
+
+        if (n<=29){
+
+            while (n<=29){
+                System.err.println("Please enter a number bigger than 30 !");
+                System.out.println("Please enter a number bigger tha 30! Thank you for your corporation :D");
+                n = scanner.nextInt();
+            }
+        }
+
+        finra(n);
+    }
+
+
+    public static void finra(int n){
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                     System.out.println("FinRa");
+                     continue;
+            } else if (i % 3 == 0) {
+                     System.out.println("Fin");
+                     continue;
+            } else if (i % 5 == 0) {
+                     System.out.println("Ra");
+                     continue;
+            }
+            System.out.println(i);
+        }
+    }
 
 }
