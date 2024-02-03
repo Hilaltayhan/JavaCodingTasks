3b346cee260f99e78497017805902da4873aec77
Almpano <bani_098@hotmail.com>
2024-02-03 16:21:37 +0000

    Created the method "oddOrEven"

--- src/albano/week1/OddOrEven.java
+++ src/albano/week1/OddOrEven.java
@@ -1,4 +1,13 @@
 package albano.week1;
 
 public class OddOrEven {
+
+
+    public static void oddOrEven(int n){
+        if (n%2==0){
+            System.out.println("The number you entered is even");
+        }else {
+            System.out.println("The number you entered is odd");
+        }
+    }
 }
