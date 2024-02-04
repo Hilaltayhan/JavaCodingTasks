394584605a92f59076346e04e64ed6c446bb8aeb
Christopher <148374799+Christopher-To@users.noreply.github.com>
2024-02-04 16:51:03 +0000

    Create Java class.

---
+++ src/Christopher/week2/SwapNumbers.java
@@ -0,0 +1,15 @@
+package Christopher.week2;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
+/*
+Numbers – Swap Numbers
+Swap two variables' values without using a third variable.*/
