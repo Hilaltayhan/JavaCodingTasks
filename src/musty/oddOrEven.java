498765570e1a2e87e134ad5e892bb16bb6db326d
musta <mustafaoguzhan93@gmail.com>
2024-02-04 19:27:02 +0000

    Added oddOrEven

--- src/musty/oddOrEven.java
+++ src/musty/oddOrEven.java
@@ -1,4 +1,19 @@
 package musty;
 
 public class oddOrEven {
+    public static void main(String[] args) {
+        number(50);
+    }
+    public static void number(int num){
+
+        if (num % 2 == 0){
+            System.out.println(num + " is even number");
+        }else System.out.println(num + " is odd number");
+    }
 }
+/*
+Write a method that can identify  a given number is even or odd.
+EX:
+       identify(5) --> "Odd"
+       identify(6) --> "Even"
+ */
