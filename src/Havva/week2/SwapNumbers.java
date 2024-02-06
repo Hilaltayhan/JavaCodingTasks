6b6cae23a26fc1ed8ab50c9446c0bc4028162e2d
havvasengel <havvasengel@yahoo.com>
2024-02-06 22:11:08 +0000

    create 3 new files

---
+++ src/Havva/week2/SwapNumbers.java
@@ -0,0 +1,24 @@
+package Havva.week2;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+
+
+        System.out.println("\n After swapping  :"  );
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+    }
+
+
+
+}
