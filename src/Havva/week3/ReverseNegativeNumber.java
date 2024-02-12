60651472d3e68dad4622060bf55aff9248f6ad3c
havvasengel <havvasengel@yahoo.com>
2024-02-12 13:33:17 +0000

    make a change on ReverseNegativeNumber

--- src/Havva/week3/ReverseNegativeNumber.java
+++ src/Havva/week3/ReverseNegativeNumber.java
@@ -2,7 +2,7 @@ package Havva.week3;
 
 public class ReverseNegativeNumber {
     public static void main(String[] args) {
-        int n = 8915;
+        int n = -8915;
         System.out.println("Original Number  :" + n);
         System.out.println("Reversed Number  :"+reverseNegativeNumber(n));
     }
@@ -10,6 +10,7 @@ public class ReverseNegativeNumber {
 
     public static int reverseNegativeNumber(int num) {
         if (num > 0) {
+
             throw new IllegalArgumentException("Number must be negative");
         }
         num = -num;
