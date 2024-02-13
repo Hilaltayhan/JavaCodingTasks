1e63b8dc76d12302005ee89b73eda9a92f244f40
hilal <hilaltayhan95@gmail.com>
2024-02-13 10:46:07 +0000

    Solved week2

---
+++ src/hilal/week2/SwapNumbers.java
@@ -0,0 +1,27 @@
+package hilal.week2;
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 10;
+
+
+        a = a + b;  //14
+        b = a - b; //4
+        a = a - b;
+
+        System.out.println("a" + a);
+        System.out.println("b" + b);
+
+
+    }
+
+
+}
+
+/*
+Create a prog that will swap two variables' values
+Do it without a third variable
+ */
