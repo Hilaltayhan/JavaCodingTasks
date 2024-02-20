9fc2fea4cd94713e8359edfc4c75cdac2af49c63
hilal <hilaltayhan95@gmail.com>
2024-02-20 11:05:49 +0000

    Solved week3 and week3

---
+++ src/hilal/week4/RemoveDublicates.java
@@ -0,0 +1,9 @@
+package hilal.week4;
+
+public class RemoveDublicates {
+}
+
+/*
+Write a return method that can remove the duplicated values from the String
+  Ex: removeDup("AAABBBCCC") ==> ABC
+ */
