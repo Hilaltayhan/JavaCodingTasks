5df23f54546a4d366958e0d0c5273a2179e7d1ce
hilal <hilaltayhan95@gmail.com>
2024-02-20 17:14:21 +0000

    Solved week2, week3 and week4

--- src/hilal/week2/ConsecutiveNumbers.java
+++ src/hilal/week2/ConsecutiveNumbers.java
@@ -1,15 +1,22 @@
 package hilal.week2;
 
 public class ConsecutiveNumbers {
+    /*
+    Numbers – Print Consecutive Numbers
+Write a function that, given a positive integer N, prints the consecutive numbers from 1 to N, each on a separate line. However, any number divisible by 2, 3 or 5 should be replaced by the word Codility, Test or Coders respectively. If a number is divisible by more than one of the numbers: 2,3 or 5, it should be replaced by a concatenation of the respective words Codility, Test and Coders in this given order.
+EX:
+Numbers divisible by both 2 and 3 should be replaced by CodilityTest and numbers divisible by all three numbers, 2,3 and 5, should be replaced by CodilityTestCoders.
 
+    */
     public static void main(String[] args) {
-        consecutiveNumbers(30);
+        int num=30;
+        consecutiveNumbers(num);
 
     }
 
-    public static void consecutiveNumbers(int n) {
+    public static void consecutiveNumbers(int number) {
         // String result = "";
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <=number; i++) {
             String result = "";
             if (i % 2 == 0) {
                 result += "Codility";
@@ -24,21 +31,9 @@ public class ConsecutiveNumbers {
                 System.out.println(i);
 
             } else {
-                System.out.println(result);
+                System.out.println( result);
             }
 
         }
     }
-
 }
-
-
-/*
-     Numbers – Print Consecutive Numbers
-Write a function that, given a positive integer N, prints the consecutive numbers from 1 to N, each on a separate line. However, any number divisible by 2, 3 or 5 should be replaced by the word Codility, Test or Coders respectively. If a number is divisible by more than one of the numbers: 2,3 or 5, it should be replaced by a concatenation of the respective words Codility, Test and Coders in this given order.
-EX:
-Numbers divisible by both 2 and 3 should be replaced by CodilityTest and numbers divisible by all three numbers, 2,3 and 5, should be replaced by CodilityTestCoders.
-
- */
-
-
--- src/hilal/week2/DivisibleBy3_5_15.java
+++ src/hilal/week2/DivisibleBy3_5_15.java
@@ -2,41 +2,33 @@ package hilal.week2;
 
 public class DivisibleBy3_5_15 {
 
+    /*
+    Write a program that can print the numbers between 1 ~ N that can be divisible by 3, 5, and 15.
+if the number can be divisible by 3, 5 and 15, then it should only be displayed in DivisibelBy15 section.
+if the number can be divisible by 3 but cannot be divisible by 15, then it should only be displayed in DivisibelBy3 section.
+if the number can be divisible by 5 but cannot be divisible by 15, then it should only be displayed in DivisibelBy5 section.
+EX:
+Input: 100
+Output: Divisible By 15 15 30 45 60 75 90
+               Divisible By 5 5 10 20 25 35 40 50 55 65 70 80 85 95 100
+               Divisible By 3 3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72 78 81 84 87 93 96 99
+     */
     public static void main(String[] args) {
-
         int n = 100;
-        String by15 = "" , by5 = "", by3 = "";
+        String by15 = "", by5 = "", by3 = "";
 
-        for (int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
 
-            if(i% 15 == 0){
+            if (i % 15 == 0) {
                 by15 += i + " ";
-
-            }else if(i % 5 == 0){
+            } else if (i % 5 == 0) {
                 by5 += i + " ";
-            }else if(i % 3 == 0){
+            } else if (i % 3 == 0) {
                 by3 += i + " ";
             }
         }
-
         System.out.println("Divisible by 15: " + by15);
-        System.out.println("by3 = " + by3);
-        System.out.println("by5 = " + by5);
-
-
+        System.out.println("Divisible by 5: " + by5);
+        System.out.println("Divisible by 3: " + by3);
     }
-
 }
-
-
-/*
-Create a program that will print all the numbers divisible 3 , 5, 15 in the range from 1 to the given N number . Print the grouped sections separately
-
-Ex:
-input : 100
-output:
- Divisible By 15 : 15 30 45 60 75 90
- Divisible By 5 : 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100
- Divisible By 3 : 3 6 9 12 15 18 21 24 27 30 33 36 39 42 48 51 54 57 60 63 66 69 72 78 81 84 87 93 96 99
-
- */
