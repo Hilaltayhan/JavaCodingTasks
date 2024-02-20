af22ad509bc5e6d0020eac021f5eda9152ab0f80
hilal <hilaltayhan95@gmail.com>
2024-02-20 17:14:21 +0000

    Solved week2, week3 and week4

--- src/hilal/week2/SwapNumbers.java
+++ src/hilal/week2/SwapNumbers.java
@@ -3,25 +3,21 @@ package hilal.week2;
 public class SwapNumbers {
 
     public static void main(String[] args) {
-
-        int a = 4;
-        int b = 10;
-
-
-        a = a + b;  //14
-        b = a - b; //4
+        int a=10,b=20;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        a = a + b;
+        b = a - b;
         a = a - b;
 
-        System.out.println("a" + a);
-        System.out.println("b" + b);
+
+
+        System.out.println("\n After swapping  :"  );
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
 
 
     }
-
-
 }
-
-/*
-Create a prog that will swap two variables' values
-Do it without a third variable
- */
--- src/hilal/week3/PrimeNumber.java
+++ src/hilal/week3/PrimeNumber.java
@@ -1,40 +1,24 @@
 package hilal.week3;
 
 public class PrimeNumber {
-
     public static void main(String[] args) {
 
-
-        System.out.println("Prime number" + isPrime(14));
-
-    }
-
-
-
-
-
-
-
-
-    private static boolean isPrime(int number){
-        if(number < 2){
-            return false;
-        }
-
-        for (int i= 2 ; i < number ; i++){
-
-            if(number % i == 0){
-                return false;
+        int num = 29;
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                flag = true;
+                break;
             }
         }
-
-        return true;
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
     }
 }
-
-
-
 /*
-Numbers -- PrimeNumber
+ Numbers -- PrimeNumber
 Write a method that can check if a number is prime or not
-*/
+ */
+
