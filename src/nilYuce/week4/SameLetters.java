2d53ddccbf94a6375ea8563226cee6906d4b612d
nilgunCydeo <nyucedal@gmail.com>
2024-02-21 15:39:32 +0000

    add if statement

--- src/nilYuce/week4/SameLetters.java
+++ src/nilYuce/week4/SameLetters.java
@@ -14,6 +14,10 @@ public class SameLetters {
 
     public static boolean same(String str1, String str2) {
 
+        if(str1.length() != str2.length()){
+            return false;
+        }
+
         char[] charArray1 = str1.toCharArray();
         char[] charArray2 = str2.toCharArray();
         Arrays.sort(charArray1);
