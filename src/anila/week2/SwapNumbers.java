98baf14f177375ee8b493debd5d35e4879ff483e
anilabaig <anila_ukcbc2011@yahoo.co.uk>
2024-03-18 16:17:38 +0000

    week 2 task update

---
+++ src/anila/week2/SwapNumbers.java
@@ -0,0 +1,26 @@
+package anila.week2;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
+/*
+Explanation
+Update a to hold the sum of its original value and the value of b.
+Update b to hold the result of subtracting its original value from the updated value of a.
+Update a to hold the result of subtracting the new value of b from the updated value of a.
+ */
+// Numbers – Swap Numbers
+//Swap two variables' values without using a third variable.
+
+
