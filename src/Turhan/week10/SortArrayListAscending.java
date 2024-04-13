eddc83463c92489f8eb6881b94de6e1662c3f91a
Turhan Ismailov <turhan.ismailov@gmail.com>
2024-04-13 22:01:46 +0100

    week10

---
+++ src/Turhan/week10/SortArrayListAscending.java
@@ -0,0 +1,36 @@
+package Turhan.week10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SortArrayListAscending {
+
+    //ArrayList - sorting in ascending
+    //Write a method that can sort the ArrayList in Ascending order without using the sort method.
+    public static void sortArrayListAscending(ArrayList<Integer> arrayList) {
+
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            for (int j = 0; j < arrayList.size() - i - 1; j++) {
+                if (arrayList.get(j) > arrayList.get(j + 1)) {
+
+                    // Swapping elements
+                    int temp = arrayList.get(j);
+                    arrayList.set(j, arrayList.get(j + 1));
+                    arrayList.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 3, 8, 1, 2, 9));
+
+        // Sort the ArrayList in ascending order
+        sortArrayListAscending(numbers);
+
+        System.out.println("Original List = " + numbers.toString());
+
+        // Print the sorted list
+        System.out.println("Sorted List: " + numbers);
+    }
+}
